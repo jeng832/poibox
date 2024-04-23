065c0da356ab17c76809c460ab6ee617e8d40190
ijeee <jeng832@gmail.com>
2024-04-23 21:04:59 +0900

    add @ExcelProperty

---
+++ src/main/java/com/github/jeng832/annotation/ExcelProperty.java
@@ -0,0 +1,13 @@
+package com.github.jeng832.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExcelProperty {
+
+    String value() default "";
+}
